1249888ce9e7f14e84f18fb506e0c5dc2df33633
Cart47 <IanGauthier@Ians-MacBook-Air.local>
2015-02-12 07:49:05 -0500

    Initial Setup
    Just the beginning stages

---
+++ Index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>Chorus in the Forest Homepage</title>
+  <meta name="author" content="name">
+  <meta name="description" content="description here">
+  <meta name="keywords" content="keywords,here">
+  <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+ </head>
+ <body>
+	 	<div class="col-lg-offset-1 col-lg-10">
+			<div class="col-lg-offset-2 navbar-right navbar-header col-lg-2">
+			 	<a href="#" class="navbar-text">Login</a>
+			 	<a href="#" class="navbar-text">Register</a>
+			</div>
+	 		<div class="col-lg-12 jumbotron">
+		 		<h1 class="col-lg-10"> Chorus in the Forest Music Festival</h1>
+		 		<h2 class=" col-lg-8"> A Celebration of 90's Music and Cat Videos</h2>
+		 		<img src="img/Mandarin.duck.arp.jpg" class="col-lg-12" />
+	 		</div>
+	 		<div class="navbar-header col-lg-6">
+			 	<a href="#" class="navbar-text">Home</a>
+			 	<a href="#" class="navbar-text">Schedule</a>
+			 	<a href="#" class="navbar-text">Events</a>
+			 	<a href="#" class="navbar-text">The Bands</a>
+			 	<a href="#" class="navbar-text">Camping Location</a>
+			 	<a href="#" class="navbar-text">Our Sponsors</a>
+			</div>
+
+	 	</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>	
+  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script>
+ </body>
+</html>
