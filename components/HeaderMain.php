76ad0716cf7255cd38e3d5c5ea89483a28a37c91
Cart47 <IanGauthier@me.com>
2015-02-24 13:11:11 -0500

    changes

--- components/HeaderMain.php
+++
@@ -1,3 +0,0 @@
-<img src="img/ChorusLogo-black.png" alt="" id="leafNote"/>
-<img src="img/CITF-textLogo.png" alt="" id="noteText"/>
-<p id="para">^^ Text Logo I made quickly.<br/><br/> <-   Actual Logo I thought was pretty cool</p>
